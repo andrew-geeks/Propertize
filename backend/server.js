a1e7a9e31cff42104d04051e7ca054b6548c1cec
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-01-22 08:34:56 +0530

    main

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = 4000;
 
 app.use(cors());
