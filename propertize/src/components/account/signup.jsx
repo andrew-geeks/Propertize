cfffcf1034d578fe60bc303a097da46d1199ea6a
Andrew George Issac <andrewgeorgeissac@gmail.com>
2023-02-02 19:16:17 +0530

    Update signup.jsx

--- propertize/src/components/account/signup.jsx
+++ propertize/src/components/account/signup.jsx
@@ -4,8 +4,6 @@ let image = require("../../images/logo.png");
 
 function Signup(){
 
-    
-
     return (
         <section class="bg-white-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 auth-sec">
